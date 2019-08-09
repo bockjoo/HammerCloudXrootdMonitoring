0c0c6f4f88754b4df4f3bf0916e8d91c2deec04c
Bockjoo Kim <bockjoo@phys.ufl.edu>
2019-08-08 20:22:09 -0400

    Update ftl_check_hc_xrootd.sh

--- ftl_check_hc_xrootd.sh
+++ ftl_check_hc_xrootd.sh
@@ -5,6 +5,7 @@ if [ "x$thesite" == "x" ] ; then
    exit 1
 fi
 query_json=es.q.match_hc_xrootd.json
+sed -e "s#@@thesite@@#$thesite#" ${query_json}.in > $query_json
 eval $(sed 's#"DBID=#\n"DBID=#' es.q.match_hc_xrootd.json | grep ^\"DBID= | cut -d\" -f2)
 
 output=$(curl -H "Content-Type: application/x-ndjson" -H "Authorization: Bearer $(cat token.txt)" -XGET https://monit-grafana.cern.ch/api/datasources/proxy/${DBID}/_msearch --data-binary "@${query_json}" 2>/dev/null | sed 's#}\]}},{"key":"#\n#g' | grep ${thesite}\")
@@ -12,11 +13,13 @@ output=$(curl -H "Content-Type: application/x-ndjson" -H "Authorization: Bearer 
 tasks=$(printf "$output\n" | cut -d\" -f1)
 for task in $tasks ; do
   completed=$(printf "$output\n"  | grep $task | cut -d\" -f4 | cut -d: -f2 | cut -d, -f1)
-  keyscounts=$(printf "$output\n" | grep $task | cut -d\" -f13- | sed 's#}# #g')
+  #keyscounts=$(printf "$output\n" | grep $task | cut -d\" -f13- | sed 's#}# #g')
+  keyscounts=$(printf "$output\n" | grep $task | sed 's#$task#\n$task#' | grep $task | cut -d\" -f13- | sed 's#\[# #' | sed 's#}# #g' | sed 's#key"#\nkey"#g')
   sum_exit_0=0
   sum_exit_not_0=0
   for keycount in $keyscounts ; do
       echo $keycount | grep -q key\": || continue
+      echo $keycount | grep -q $task && continue
       key=$(echo $keycount | cut -d: -f2 | cut -d, -f1)
       count=$(echo $keycount | cut -d: -f3)
       if [ $key -eq 0 ] ; then
