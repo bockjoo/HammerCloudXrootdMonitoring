4f5d3d5fd25d11218dfcd3a06bb64fd41014e948
Bockjoo Kim <bockjoo@phys.ufl.edu>
2019-08-09 12:33:25 -0400

    Update ftl_check_hc_xrootd.sh

--- ftl_check_hc_xrootd.sh
+++ ftl_check_hc_xrootd.sh
@@ -12,10 +12,14 @@ eval $(sed 's#"DBID=#\n"DBID=#' es.q.match_hc_xrootd.json | grep ^\"DBID= | cut 
 output=$(curl -H "Content-Type: application/x-ndjson" -H "Authorization: Bearer $(cat token.txt)" -XGET https://monit-grafana.cern.ch/api/datasources/proxy/${DBID}/_msearch --data-binary "@${query_json}" 2>/dev/null | sed 's#"CRAB_Workflow"#\n"CRAB_Workflow"#' | sed 's#}\]}},{"key":"#\n#g' | sed 's#"key":"#\n#' | grep ${thesite}\")
 
 tasks=$(printf "$output\n" | cut -d\" -f1)
+sum_0=0
+sum_not_0=0
+sum_completed=0
 for task in $tasks ; do
   completed=$(printf "$output\n"  | grep $task | cut -d\" -f4 | cut -d: -f2 | cut -d, -f1)
   #keyscounts=$(printf "$output\n" | grep $task | cut -d\" -f13- | sed 's#}# #g')
   keyscounts=$(printf "$output\n" | grep $task | sed 's#$task#\n$task#' | grep $task | cut -d\" -f13- | sed 's#\[# #' | sed 's#}# #g' | sed 's#key"#\nkey"#g')
+  sum_completed=$(expr $sum_completed + $completed)
   sum_exit_0=0
   sum_exit_not_0=0
   for keycount in $keyscounts ; do
@@ -25,8 +29,10 @@ for task in $tasks ; do
       count=$(echo $keycount | cut -d: -f3)
       if [ $key -eq 0 ] ; then
          sum_exit_0=$(expr $sum_exit_0 + $count)
+         sum_0=$(expr $sum_0 + $count)
       else
          sum_exit_not_0=$(expr $sum_exit_not_0 + $count)
+         sum_not_0=$(expr $sum_not_0 + $count)
       fi
   done
   sum=$(expr $sum_exit_not_0 + $sum_exit_0)
@@ -36,6 +42,9 @@ for task in $tasks ; do
      echo Warning $completed is not equal to $sum
   fi  
 done
+sum=$(expr $sum_not_0 + $sum_0)
+success_rate=$(echo "scale=2 ; $sum_0 / $sum " | bc)
+echo alltasks sum_completed=$sum_completed sum=$sum success=$success_rate
 exit 0
 
 
